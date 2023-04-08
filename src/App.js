e2c7274d031b6176435cdbb0f5ef92462a38e6c9
GabrielaSernaBanks <gserna88@gmail.com>
2023-04-08 13:31:14 -0400

    Editing for desktop view

--- src/App.js
+++ src/App.js
@@ -1,33 +1,43 @@
 import Nav from "./components/Nav/Nav";
 import MainVideo from "./components/MainVideo/MainVideo";
 import VideoDetails from "./components/VideoDetails/VideoDetails";
-import React, {useState} from "react";
-import videoInfo from './data/video-details.json'
+import React, { useState } from "react";
+import videoInfo from "./data/video-details.json";
 import CommentsContainer from "./components/CommentsContainer/CommentsContainer";
 import PreviewVideoContainer from "./components/PreviewVideoContainer/PreviewVideoContainer";
-
+import "./App.scss";
 
 function App() {
   const [videoIndex, setVideoIndex] = useState(0);
 
   const selectedVideo = videoInfo.filter(
-    (video) => video.id !== videoInfo[videoIndex].id);
+    (video) => video.id !== videoInfo[videoIndex].id
+  );
 
-    const videoClicked = (index) => {
-      setVideoIndex(index + 1);
-      console.log(index);
-    };
+  const videoClicked = (index) => {
+    setVideoIndex(index + 1);
+    console.log(index);
+  };
 
   return (
     <div>
       <Nav />
       <MainVideo videoInfo={videoInfo} videoIndex={videoIndex} />
-      <VideoDetails videoInfo={videoInfo} videoIndex={videoIndex}/>
-      <CommentsContainer videoInfo={videoInfo} videoIndex={videoIndex} />
-      <PreviewVideoContainer videoInfo={selectedVideo} videoIndex={videoIndex} clickHandler={videoClicked}/>
+      <div className="app__desktopDivMain">
+        <div className="app__desktopDivDetails">
+          <VideoDetails videoInfo={videoInfo} videoIndex={videoIndex} />
+          <CommentsContainer videoInfo={videoInfo} videoIndex={videoIndex} />
+        </div>
+        <div className="app__previewVideoContainer">
+          <PreviewVideoContainer
+            videoInfo={selectedVideo}
+            videoIndex={videoIndex}
+            clickHandler={videoClicked}
+          />
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
-
