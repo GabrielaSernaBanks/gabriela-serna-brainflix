fbe180da63cd54645c5a2090a4bcffd1b1fd3f52
GabrielaSernaBanks <gserna88@gmail.com>
2023-04-13 00:01:00 -0400

    Editing main video image source

--- src/pages/MainVideo/MainVideo.js
+++ src/pages/MainVideo/MainVideo.js
@@ -11,25 +11,27 @@ const ApiKey = '?api_key=42e9c01f-8636-431d-964b-dbdc0c73435c'
 
 function MainVideo (){
 
-  const [videoImage, setVideoImage] = useState('image');
-  const [videoImages, setVideoImages] = useState([]);
+  const [videoImage, setVideoImage] = useState();
+  const [videoData, setVideoData] = useState([]);
+
   const changeHandler = (event) => {
     const selectVideoImage = event.target;
-    setVideoImage(videoImage.image)
+    setVideoImage(selectVideoImage.image);
   }
 
-  useEffect(()=>{
+  useEffect(()=> {
     axios
     .get(`${baseUrl}/videos${ApiKey}`)
     .then((response)=>{
-      setVideoImages(response.data);
+      setVideoData(response.image);
     })
     }, []);
 
     return (
       <div className='mainVideo__container'>
         <video className="mainVideo__image" controls>
-          
+          <source src='{videoData.image}'  />
+          <img  />
         </video>
         <div className="app__desktopDivMain">
           <div className="app__desktopDivDetails">
