b1564923110dc34df79419fc17afab57c0c079f2
GabrielaSernaBanks <gserna88@gmail.com>
2023-04-16 15:19:05 -0400

    Edited styling for upload page mobile version

--- src/pages/UploadPage/UploadPage.js
+++ src/pages/UploadPage/UploadPage.js
@@ -4,49 +4,54 @@ import thumbnail from '../../assets/Images/Upload-video-preview.jpg'
 
 function UploadPage() {
 
-  return (
+	return (
 
-    <div className='uploadPage'>
-      <h1 className='uploadPage__header'>Upload Video</h1>
-      <img src={thumbnail} />
-      <div className='uploadPage__video-container'>
-        <p>VIDEO THUMBNAIL</p>
-        {/* <video className="mainVideo__image" poster={videoInfo[videoIndex].image} controls></video> */}
-      </div>
-      <div className='uploadPage__form-container'>
-      <form className="uploadPage__form">
-            <div className="uploadPage__formInput">
-            <label className="uploadPage__title" for="comment">
-            TITLE YOUR VIDEO
-            </label>
-            <input
-            required
-            type="text"
-            className="uploadPage__video-title"
-            id="newVideo"
-            name="title"
-            placeholder="Add a title to your video"
-            ></input>
-            <input
-            required
-            type="text"
-            className="uploadPage__video-description"
-            id="newVideo"
-            name="description"
-            placeholder="Add a description to your video"
-            ></input>
-            </div>
-            <button className="uploadPage__button">
-            <img className='uploadPage__button-icon' src={publishIcon}></img>
-            <span className="uploadPage__button-text">PUBLISH</span>
-            </button>
-            <p>CANCEL</p>
-            {/* <button className="uploadPage__button-cancel">CANCEL
-            </button> */}
-        </form>
-      </div>
-    </div>
-  );
+		<div className='uploadPage'>
+			<div className='uploadPage__header-container'>
+				<h1 className='uploadPage__header'>Upload Video</h1>
+			</div>
+			<div className='uploadPage__image-container'>
+				<p className='uploadPage__subheader'>VIDEO THUMBNAIL</p>
+				<img className='uploadPage__thumbnail' src={thumbnail} />
+			</div>
+			<div className='uploadPage__form-container'>
+				<form className="uploadPage__form">
+					<div className="uploadPage__formInput">
+						<label className="uploadPage__title" htmlFor="comment">
+							TITLE YOUR VIDEO
+						</label>
+						<input
+							required
+							type="text"
+							className="uploadPage__video-title"
+							id="newVideo"
+							name="title"
+							placeholder="Add a title to your video"
+						></input>
+						<label className="uploadPage__description" htmlFor="comment">
+							ADD A VIDEO DESCRIPTION
+						</label>
+						<input
+							required
+							type="text"
+							className="uploadPage__video-description"
+							id="newVideo"
+							name="description"
+							placeholder="Add a description to your video"
+						></input>
+					</div>
+					<div className='uploadPage__buttons'>
+						<button className="uploadPage__button">
+							<img className='uploadPage__button-icon' src={publishIcon}></img>
+							<span className="uploadPage__button-text">PUBLISH</span>
+						</button>
+						<button className="uploadPage__button-cancel">CANCEL
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 }
 
 export default UploadPage;
